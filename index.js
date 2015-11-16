c2f92bdf9c0f92a672df4938de9c762b7342b501
Gant <GantMan@gmail.com>
2015-11-15 18:25:46 -0600

    new name

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 #! /usr/bin/env node
 'use strict'
 
-const cow = `
-                    ___
-          (__)    /    \\
-          (oo)   ( noo! )
-   /-------\\/  --'\\____/
-  / |     ||
- *  ||----||
-    ^^    ^^
-`
-console.log(cow)
+console.log('irrigate')
