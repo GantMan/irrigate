79fd8d91a2118ce9cd103380674b15c3aa64def2
Gant <GantMan@gmail.com>
2015-11-16 22:45:29 -0600

    adding functional component generator - needs refactoring

--- component/index.js
+++ component/index.js
@@ -8,6 +8,10 @@ var _safe = require('colors/safe');
 
 var _safe2 = _interopRequireDefault(_safe);
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
 var _yeomanGenerator = require('yeoman-generator');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -17,6 +21,33 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var baseComponentContent = '\'use strict\'\n\nimport React, { ScrollView, View, Text } from \'react-native\'\nvar styles = require(\'../Styles/SomeScreenStyle\')\n\nexport default class SomeScreenStyles extends React.Component {\n\n  static propTypes = {\n    navigator: React.PropTypes.object\n  }\n\n  render () {\n    return (\n      <ScrollView style={styles.container}>\n        <Text>Some Component</Text>\n      </ScrollView>\n    )\n  }\n}\n';
+
+var baseComponentStyle = '\'use strict\'\n\nimport { StyleSheet } from \'react-native\'\nimport { Fonts, Colors, Metrics } from \'../Themes/\'\n\nexport default StyleSheet.create({\n  container: {\n    flex: 1,\n    paddingTop: Metrics.titlePadding\n  }\n})\n';
+
+var fileSign = function fileSign(path) {
+  var separator = _safe2.default.rainbow('-=-=-=-=-=-=-');
+  console.log(separator + _safe2.default.yellow(' "') + _safe2.default.underline(path) + _safe2.default.yellow('" was saved') + separator);
+};
+
+var createComponent = function createComponent(folder, fileName) {
+  var fullFile = './App/' + folder + '/' + fileName + '.js';
+  var fullStyleFile = './App/Styles/' + fileName + 'Style.js';
+  _fs2.default.writeFile(fullFile, baseComponentContent, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    fileSign(fullFile);
+  });
+
+  _fs2.default.writeFile(fullStyleFile, baseComponentStyle, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    fileSign(fullStyleFile);
+  });
+};
 
 var ComponentGenerator = (function (_NamedBase) {
   _inherits(ComponentGenerator, _NamedBase);
@@ -31,6 +62,8 @@ var ComponentGenerator = (function (_NamedBase) {
     key: 'generateComponent',
     value: function generateComponent() {
       console.log(_safe2.default.yellow('irrigate component - ' + this.name));
+      console.log(_safe2.default.red('Creating Component'));
+      createComponent('Components', this.name);
     }
   }]);
 
--- src/component/index.es
+++ src/component/index.es
@@ -2,12 +2,72 @@
 'use strict'
 
 import colors from 'colors/safe'
+import fs from 'fs'
 import { NamedBase } from 'yeoman-generator'
+
+let baseComponentContent = `'use strict'
+
+import React, { ScrollView, View, Text } from 'react-native'
+var styles = require('../Styles/SomeScreenStyle')
+
+export default class SomeScreenStyles extends React.Component {
+
+  static propTypes = {
+    navigator: React.PropTypes.object
+  }
+
+  render () {
+    return (
+      <ScrollView style={styles.container}>
+        <Text>Some Component</Text>
+      </ScrollView>
+    )
+  }
+}
+`
+
+let baseComponentStyle = `'use strict'
+
+import { StyleSheet } from 'react-native'
+import { Fonts, Colors, Metrics } from '../Themes/'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Metrics.titlePadding
+  }
+})
+`
+
+const fileSign = function (path) {
+  const separator = colors.rainbow('-=-=-=-=-=-=-')
+  console.log(separator + colors.yellow(' "') + colors.underline(path) + colors.yellow('" was saved') + separator)
+}
+
+const createComponent = function (folder, fileName) {
+  var fullFile = `./App/${folder}/${fileName}.js`
+  var fullStyleFile = `./App/Styles/${fileName}Style.js`
+  fs.writeFile(fullFile, baseComponentContent, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    fileSign(fullFile)
+  })
+
+  fs.writeFile(fullStyleFile, baseComponentStyle, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+    fileSign(fullStyleFile)
+  })
+}
 
 class ComponentGenerator extends NamedBase {
 
   generateComponent () {
     console.log(colors.yellow('irrigate component - ' + this.name))
+    console.log(colors.red('Creating Component'))
+    createComponent('Components', this.name)
   }
 }
 
