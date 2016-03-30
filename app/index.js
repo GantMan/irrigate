986afe96ddfb837e209d319a3657b676f0f0e332
Gant <GantMan@gmail.com>
2016-03-30 18:25:18 -0500

    almost there

--- app/index.js
+++ app/index.js
@@ -14,10 +14,6 @@ var _shelljs = require('shelljs');
 
 var _shelljs2 = _interopRequireDefault(_shelljs);
 
-var _mkdirp = require('mkdirp');
-
-var _mkdirp2 = _interopRequireDefault(_mkdirp);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -26,12 +22,67 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var TEMP_FOLDER = 'baseTemp';
-var templateGrab = function templateGrab(name, dir) {
-  return _shelljs2.default.cp('-R', name + '/' + TEMP_FOLDER + '/' + dir, name + '/' + dir);
+var verifyTools = function verifyTools() {
+  // verify react-native
+  if (!_shelljs2.default.which('react-native')) {
+    console.log(_safe2.default.red('This script requires react-native to be installed first.'));
+    _shelljs2.default.exit(1);
+  }
+
+  // Warn if outdated
+  _shelljs2.default.exec('npm outdated react-native-cli');
+
+  // verify git
+  if (!_shelljs2.default.which('git')) {
+    console.log(_safe2.default.red('This script requires git to be installed first.'));
+    _shelljs2.default.exit(1);
+  }
+
+  // verify rnpm
+  if (!_shelljs2.default.which('rnpm')) {
+    console.log(_safe2.default.red('This script requires rnpm to be installed.'));
+    _shelljs2.default.exec('npm i -g rnpm');
+  }
+
+  // Warn if outdated
+  _shelljs2.default.exec('npm outdated rnpm');
 };
-var templatePut = function templatePut(gitRoot, templateRoot, file) {
-  return _shelljs2.default.cp(gitRoot + '/' + file, templateRoot + '/');
+
+// const makeDirectories = (name) => {
+//   mkdirp(`${name}/${TEMP_FOLDER}`, (err) => {
+//     if (err) {
+//       console.log(err)
+//       Shell.exit(1)
+//     }
+//   })
+// }
+
+var copyOverBase = function copyOverBase(context) {
+  // copy a Readme template not ours
+  // TODO
+
+  // copy package.json
+  context.fs.copyTpl(context.templatePath('package.json.template'), context.destinationPath(context.name + '/package.json'), { name: context.name });
+
+  // copy template of index.ios.js
+  context.fs.copyTpl(context.templatePath('index.js.template'), context.destinationPath(context.name + '/index.ios.js'), { name: context.name });
+
+  // copy template of index.android.js
+  context.fs.copyTpl(context.templatePath('index.js.template'), context.destinationPath(context.name + '/index.android.js'), { name: context.name });
+
+  // copy git_hooks/
+  context.directory(context.templatePath('git_hooks'), context.destinationPath(context.name + '/git_hooks'));
+
+  // copy Tests/
+  context.directory(context.templatePath('Tests'), context.destinationPath(context.name + '/Tests'));
+
+  // copy App/
+  context.directory(context.templatePath('App'), context.destinationPath(context.name + '/App'));
+};
+
+var emptyFolder = function emptyFolder(folder) {
+  _shelljs2.default.rm('-rf', folder);
+  _shelljs2.default.mkdir(folder);
 };
 
 var AppGenerator = function (_NamedBase) {
@@ -44,114 +95,34 @@ var AppGenerator = function (_NamedBase) {
   }
 
   _createClass(AppGenerator, [{
-    key: 'verifyTools',
-    value: function verifyTools() {
-      // verify react-native
-      if (!_shelljs2.default.which('react-native')) {
-        console.log(_safe2.default.red('This script requires react-native to be installed first.'));
-        _shelljs2.default.exit(1);
-      }
-
-      // Warn if outdated
-      _shelljs2.default.exec('npm outdated react-native-cli');
-
-      // verify git
-      if (!_shelljs2.default.which('git')) {
-        console.log(_safe2.default.red('This script requires git to be installed first.'));
-        _shelljs2.default.exit(1);
-      }
-
-      // verify rnpm
-      if (!_shelljs2.default.which('rnpm')) {
-        console.log(_safe2.default.red('This script requires rnpm to be installed.'));
-        _shelljs2.default.exec('npm i -g rnpm');
-      }
-
-      // Warn if outdated
-      _shelljs2.default.exec('npm outdated rnpm');
-    }
-  }, {
-    key: 'makeDirectories',
-    value: function makeDirectories(name) {
-      (0, _mkdirp2.default)(name + '/' + TEMP_FOLDER, function (err) {
-        if (err) {
-          console.log(err);
-          _shelljs2.default.exit(1);
-        }
-      });
-    }
-
-    // worth noting but requires template
-    // this.bulkCopy(`./${this.name}/${TEMP_FOLDER}/App`, `./${this.name}/App`)
-
-  }, {
-    key: 'copyOver',
-    value: function copyOver() {
-      // // copy package.json
-      // this.fs.copyTpl(
-      //   this.templatePath('package.json.template'),
-      //   this.destinationPath(`${this.name}/package.json`),
-      //   { name: this.name }
-      // )
-
-      // // copy template of index.ios.js
-      // this.fs.copyTpl(
-      //   this.templatePath('index.js.template'),
-      //   this.destinationPath(`${this.name}/index.ios.js`),
-      //   { name: this.name }
-      // )
-
-      // // copy template of index.android.js
-      // this.fs.copyTpl(
-      //   this.templatePath('index.js.template'),
-      //   this.destinationPath(`${this.name}/index.android.js`),
-      //   { name: this.name }
-      // )
-
-      // copy git_hooks/
-      templateGrab(this.name, 'git_hooks');
-      // copy Tests/
-      templateGrab(this.name, 'Tests');
-      // copy App/
-      templateGrab(this.name, 'App');
-      // copy a Readme not ours
-      // TODO
-    }
-  }, {
     key: 'generateApp',
     value: function generateApp() {
+      var templateFolder = this.sourceRoot();
       console.log(_safe2.default.yellow('irrigate app -> ') + this.name + ' ☕️ This will take a while ☕️ ');
-      var templateRoot = this.sourceRoot();
-      var gitRoot = this.name + '/' + TEMP_FOLDER;
       // force overwrite on conflicts (default is ask user)
       this.conflicter.force = true;
 
       // Fail if tools are missing
-      // this.verifyTools()
-      // // No clue why this is needed
-      // console.log('When directory already exists, please type `yes` to continue.')
-      // // Create latest RN project
-      // this.spawnCommandSync('react-native', ['init', this.name])
-      // // ensure temp dir
-      // this.makeDirectories(this.name)
-      // // Grab latest RNBase
-      // Shell.exec(`git clone git@github.com:infinitered/react_native_base.git ${gitRoot}`)
+      verifyTools();
+      // Clean template folder
+      emptyFolder(templateFolder);
+      // Create latest RN project
+      this.spawnCommandSync('react-native', ['init', this.name]);
 
-      // templatePut(gitRoot, templateRoot, `package.json`)
-      // this.copyOver.bind(this)
+      // Grab latest RNBase into templates folder
+      _shelljs2.default.exec('git clone git@github.com:infinitered/react_native_base.git ' + templateFolder);
 
-      // npm install new package.json via `npm --prefix ./some_project install ./some_project`
-      // this.spawnCommandSync('npm', ['--prefix', `./${this.name}`, 'install', `./${this.name}`])
-      // WORKS TO HERE
-      _shelljs2.default.exec('cd ' + this.name + ' && rnpm link');
+      // Copy over files from RN Base that apply
+      copyOverBase(this);
 
+      // npm install copied package.json via `npm --prefix ./some_project install ./some_project`
+      this.spawnCommandSync('npm', ['--prefix', './' + this.name, 'install', './' + this.name]);
       // Do rnpm link
-      // Cleanup
+      _shelljs2.default.exec('cd ' + this.name + ' && rnpm link');
+      // Clean template folder
+      emptyFolder(templateFolder);
 
-      console.log('Time to get cooking!');
-
-      // react-native init this.name
-      // git clone git@github.com:infinitered/react_native_base.git this.name/TEMP_FOLDER
+      console.log('Time to get cooking! ' + _safe2.default.red('IRrigate is Done!'));
     }
   }]);
 
--- src/app/index.es
+++ src/app/index.es
@@ -4,112 +4,119 @@
 import colors from 'colors/safe'
 import { NamedBase } from 'yeoman-generator'
 import Shell from 'shelljs'
-import mkdirp from 'mkdirp'
 
-const TEMP_FOLDER = 'baseTemp'
-const templateGrab = (name, dir) => Shell.cp('-R', `${name}/${TEMP_FOLDER}/${dir}`, `${name}/${dir}`)
-const templatePut = (gitRoot, templateRoot, file) => Shell.cp(`${gitRoot}/${file}`, `${templateRoot}/`)
+const verifyTools = () => {
+  // verify react-native
+  if (!Shell.which('react-native')) {
+    console.log(colors.red('This script requires react-native to be installed first.'))
+    Shell.exit(1)
+  }
+
+  // Warn if outdated
+  Shell.exec(`npm outdated react-native-cli`)
+
+  // verify git
+  if (!Shell.which('git')) {
+    console.log(colors.red('This script requires git to be installed first.'))
+    Shell.exit(1)
+  }
+
+  // verify rnpm
+  if (!Shell.which('rnpm')) {
+    console.log(colors.red('This script requires rnpm to be installed.'))
+    Shell.exec('npm i -g rnpm')
+  }
+
+  // Warn if outdated
+  Shell.exec(`npm outdated rnpm`)
+}
+
+// const makeDirectories = (name) => {
+//   mkdirp(`${name}/${TEMP_FOLDER}`, (err) => {
+//     if (err) {
+//       console.log(err)
+//       Shell.exit(1)
+//     }
+//   })
+// }
+
+const copyOverBase = (context) => {
+  // copy a Readme template not ours
+  // TODO
+
+  // copy package.json
+  context.fs.copyTpl(
+    context.templatePath('package.json.template'),
+    context.destinationPath(`${context.name}/package.json`),
+    { name: context.name }
+  )
+
+  // copy template of index.ios.js
+  context.fs.copyTpl(
+    context.templatePath('index.js.template'),
+    context.destinationPath(`${context.name}/index.ios.js`),
+    { name: context.name }
+  )
+
+  // copy template of index.android.js
+  context.fs.copyTpl(
+    context.templatePath('index.js.template'),
+    context.destinationPath(`${context.name}/index.android.js`),
+    { name: context.name }
+  )
+
+  // copy git_hooks/
+  context.directory(
+    context.templatePath('git_hooks'),
+    context.destinationPath(`${context.name}/git_hooks`)
+  )
+
+  // copy Tests/
+  context.directory(
+    context.templatePath('Tests'),
+    context.destinationPath(`${context.name}/Tests`)
+  )
+
+  // copy App/
+  context.directory(
+    context.templatePath('App'),
+    context.destinationPath(`${context.name}/App`)
+  )
+}
+
+const emptyFolder = (folder) => {
+  Shell.rm('-rf', folder)
+  Shell.mkdir(folder)
+}
 
 class AppGenerator extends NamedBase {
-
-  verifyTools () {
-    // verify react-native
-    if (!Shell.which('react-native')) {
-      console.log(colors.red('This script requires react-native to be installed first.'))
-      Shell.exit(1)
-    }
-
-    // Warn if outdated
-    Shell.exec(`npm outdated react-native-cli`)
-
-    // verify git
-    if (!Shell.which('git')) {
-      console.log(colors.red('This script requires git to be installed first.'))
-      Shell.exit(1)
-    }
-
-    // verify rnpm
-    if (!Shell.which('rnpm')) {
-      console.log(colors.red('This script requires rnpm to be installed.'))
-      Shell.exec('npm i -g rnpm')
-    }
-
-    // Warn if outdated
-    Shell.exec(`npm outdated rnpm`)
-  }
-
-  makeDirectories (name) {
-    mkdirp(`${name}/${TEMP_FOLDER}`, (err) => {
-      if (err) {
-        console.log(err)
-        Shell.exit(1)
-      }
-    })
-  }
-
-  copyOver () {
-    // copy package.json
-    this.fs.copyTpl(
-      this.templatePath('package.json.template'),
-      this.destinationPath(`${this.name}/package.json`),
-      { name: this.name }
-    )
-
-    // copy template of index.ios.js
-    this.fs.copyTpl(
-      this.templatePath('index.js.template'),
-      this.destinationPath(`${this.name}/index.ios.js`),
-      { name: this.name }
-    )
-
-    // copy template of index.android.js
-    this.fs.copyTpl(
-      this.templatePath('index.js.template'),
-      this.destinationPath(`${this.name}/index.android.js`),
-      { name: this.name }
-    )
-
-    // copy git_hooks/
-    templateGrab(this.name, 'git_hooks')
-    // copy Tests/
-    templateGrab(this.name, 'Tests')
-    // copy App/
-    templateGrab(this.name, 'App')
-    // copy a Readme not ours
-    // TODO
-  }
-
   generateApp () {
+    const templateFolder = this.sourceRoot()
     console.log(colors.yellow('irrigate app -> ') + this.name + ' ☕️ This will take a while ☕️ ')
-    const gitRoot = `${this.name}/${TEMP_FOLDER}`
     // force overwrite on conflicts (default is ask user)
     this.conflicter.force = true
 
-    // // Fail if tools are missing
-    // this.verifyTools()
-    // // No clue why this is needed
-    // console.log('When directory already exists, please type `yes` to continue.')
-    // // Create latest RN project
-    // this.spawnCommandSync('react-native', ['init', this.name])
-    // // ensure temp dir
-    // this.makeDirectories(this.name)
-    // // Grab latest RNBase
-    // Shell.exec(`git clone git@github.com:infinitered/react_native_base.git ${gitRoot}`)
-    Shell.exec(`git clone git@github.com:infinitered/react_native_base.git ${this.sourceRoot()}`)
-    // // templatePut(gitRoot, this.sourceRoot(), `package.json`)
-    // this.copyOver.bind(this)
+    // Fail if tools are missing
+    verifyTools()
+    // Clean template folder
+    emptyFolder(templateFolder)
+    // Create latest RN project
+    this.spawnCommandSync('react-native', ['init', this.name])
 
-    // // npm install new package.json via `npm --prefix ./some_project install ./some_project`
-    // this.spawnCommandSync('npm', ['--prefix', `./${this.name}`, 'install', `./${this.name}`])
-    // // Do rnpm link
-    // Shell.exec(`cd ${this.name} && rnpm link`)
+    // Grab latest RNBase into templates folder
+    Shell.exec(`git clone git@github.com:infinitered/react_native_base.git ${templateFolder}`)
 
-    //this.spawnCommandSync('react-native', ['init', this.name])
+    // Copy over files from RN Base that apply
+    copyOverBase(this)
 
-    console.log('Time to get cooking!')
+    // npm install copied package.json via `npm --prefix ./some_project install ./some_project`
+    this.spawnCommandSync('npm', ['--prefix', `./${this.name}`, 'install', `./${this.name}`])
+    // Do rnpm link
+    Shell.exec(`cd ${this.name} && rnpm link`)
+    // Clean template folder
+    // emptyFolder(templateFolder)
 
-    // react-native init this.name
-    // git clone git@github.com:infinitered/react_native_base.git this.name/TEMP_FOLDER
+    console.log('Time to get cooking! ' + colors.red('IRrigate is Done!'))
   }
 }
 
