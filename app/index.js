9ac3ae23d7128c85e9ae00e2284f99c2e3fc2c59
Gant <GantMan@gmail.com>
2016-03-30 12:01:14 -0500

    working npm for RN

--- app/index.js
+++ app/index.js
@@ -14,6 +14,10 @@ var _shelljs = require('shelljs');
 
 var _shelljs2 = _interopRequireDefault(_shelljs);
 
+var _mkdirp = require('mkdirp');
+
+var _mkdirp2 = _interopRequireDefault(_mkdirp);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -21,6 +25,8 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TEMP_FOLDER = 'baseTemp';
 
 var AppGenerator = function (_NamedBase) {
   _inherits(AppGenerator, _NamedBase);
@@ -32,20 +38,52 @@ var AppGenerator = function (_NamedBase) {
   }
 
   _createClass(AppGenerator, [{
-    key: 'generateApp',
-    value: function generateApp() {
-      console.log(_safe2.default.yellow('irrigate app - ' + this.name));
-
+    key: 'verifyTools',
+    value: function verifyTools() {
       // verify react-native
       if (!_shelljs2.default.which('react-native')) {
         console.log(_safe2.default.red('This script requires react-native to be installed first.'));
         _shelljs2.default.exit(1);
       }
 
+      // Warn if outdated
+      _shelljs2.default.exec('npm outdated react-native-cli');
+
+      // verify git
+      if (!_shelljs2.default.which('git')) {
+        console.log(_safe2.default.red('This script requires git to be installed first.'));
+        _shelljs2.default.exit(1);
+      }
+    }
+  }, {
+    key: 'makeDirectories',
+    value: function makeDirectories(name) {
+      (0, _mkdirp2.default)(name + '/' + TEMP_FOLDER, function (err) {
+        if (err) {
+          console.log(err);
+          _shelljs2.default.exit(1);
+        }
+      });
+    }
+  }, {
+    key: 'generateApp',
+    value: function generateApp() {
+      console.log(_safe2.default.yellow('irrigate app - ') + this.name + ' ☕️ This will take a while ☕️ ');
+      // Fail if tools are missing
+      this.verifyTools();
+      // No clue why this is needed
+      console.log('When directory exists, please type `yes` to continue.');
+      // Create latest RN project
+      this.spawnCommandSync('react-native', ['init', this.name]);
+      // ensure temp dir
+      this.makeDirectories(this.name);
+      // Grab latest RNBase
+      _shelljs2.default.exec('git clone git@github.com:infinitered/react_native_base.git ' + this.name + '/' + TEMP_FOLDER);
+
       console.log('Time to get cooking!');
 
       // react-native init this.name
-      _shelljs2.default.exec('ls -al');
+      // git clone git@github.com:infinitered/react_native_base.git this.name/TEMP_FOLDER
     }
   }]);
 
--- src/app/index.es
+++ src/app/index.es
@@ -4,22 +4,55 @@
 import colors from 'colors/safe'
 import { NamedBase } from 'yeoman-generator'
 import Shell from 'shelljs'
+import mkdirp from 'mkdirp'
+
+const TEMP_FOLDER = 'baseTemp'
 
 class AppGenerator extends NamedBase {
 
-  generateApp () {
-    console.log(colors.yellow('irrigate app - ' + this.name))
-
+  verifyTools () {
     // verify react-native
     if (!Shell.which('react-native')) {
       console.log(colors.red('This script requires react-native to be installed first.'))
       Shell.exit(1)
     }
 
+    // Warn if outdated
+    Shell.exec(`npm outdated react-native-cli`)
+
+    // verify git
+    if (!Shell.which('git')) {
+      console.log(colors.red('This script requires git to be installed first.'))
+      Shell.exit(1)
+    }
+  }
+
+  makeDirectories (name) {
+    mkdirp(`${name}/${TEMP_FOLDER}`, (err) => {
+      if (err) {
+        console.log(err)
+        Shell.exit(1)
+      }
+    })
+  }
+
+  generateApp () {
+    console.log(colors.yellow('irrigate app - ') + this.name + ' ☕️ This will take a while ☕️ ')
+    // Fail if tools are missing
+    this.verifyTools()
+    // No clue why this is needed
+    console.log('When directory already exists, please type `yes` to continue.')
+    // Create latest RN project
+    this.spawnCommandSync('react-native', ['init', this.name])
+    // ensure temp dir
+    this.makeDirectories(this.name)
+    // Grab latest RNBase
+    Shell.exec(`git clone git@github.com:infinitered/react_native_base.git ${this.name}/${TEMP_FOLDER}`)
+
     console.log('Time to get cooking!')
 
     // react-native init this.name
-    Shell.exec('ls -al')
+    // git clone git@github.com:infinitered/react_native_base.git this.name/TEMP_FOLDER
   }
 }
 
